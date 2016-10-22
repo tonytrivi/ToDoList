d3a6b8d0c1c218966d4dd5719fc8c88df2d6bb78
Tony Trivison <shambles72@hotmail.com>
2016-10-22 13:28:46 -0700

    styling divs in task list

--- components/tasklist.js
+++ components/tasklist.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default class TaskList extends React.Component {
     renderItems () {
         var taskList = this.props.tasks.map(function(task){
-                        return <li key={task.ID}>{task.description}</li>;
+                        return <div className="task-item-container" key={task.ID}>
+                            <div className="task-item">
+                            <span className="task-item-desc">{task.description}</span>
+                            </div>
+                        </div>;
                       })
 
         return  <ul>{ taskList }</ul>
