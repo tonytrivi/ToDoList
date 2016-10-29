2f3ffe8786f2577e73977ae4a32ad73e1b440865
Tony Trivison <shambles72@hotmail.com>
2016-10-29 08:37:48 -0700

    added delete icon

--- components/tasklistitem.js
+++ components/tasklistitem.js
@@ -6,8 +6,15 @@ export default class TaskListItem extends React.Component {
             <div className="task-item-container">
                 <div className="task-item">
                     <span className="task-item-desc">{this.props.desc}</span>
-                    <span className="task-item-desc right">{this.props.time.getMonth()}/{this.props.time.getDate()}</span>
+                    <span className="task-item-desc right">{this.props.time.getMonth()}/{this.props.time.getDate()} <img src="../resources/images/cross.svg" alt="delete" height="15" width="15" /></span>
                 </div>
+                <div>
+                    
+                </div>
+                <div>
+                    <button>edit</button>
+                    <button>delete</button>
+                </div> 
             </div>
         );
     }
