5d292abddbb39c88277ac969bae4fbbfbbe0c675
Tony Trivison <shambles72@hotmail.com>
2016-10-29 13:07:58 -0700

    added buttons and UI behavior

--- components/app.js
+++ components/app.js
@@ -39,6 +39,7 @@ export default class App extends React.Component {
         return (
             <div>
                 <TaskList tasks={this.state.tasks}/>
+                <div className="icon-credit">Icons made by <a href="http://www.flaticon.com/authors/madebyoliver" title="Madebyoliver">Madebyoliver</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
             </div>
         );
     }
--- components/tasklistitem.js
+++ components/tasklistitem.js
@@ -1,21 +1,87 @@
 import React from 'react';
 
 export default class TaskListItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isEditing: false
+        };
+    }
+
+    renderActionsSection() {
+        if (this.state.isEditing) {
+            return (
+                <span className="task-item-desc right">{this.props.time.getMonth()}/{this.props.time.getDate()} 
+                        <span> </span>
+                        <img src="../resources/images/edit_gn.svg" 
+                            className="edit-img"
+                            alt="edit" 
+                            height="15" 
+                            width="15" /> 
+                        <span> </span>
+                        <img src="../resources/images/revert.svg" 
+                            className="delete-img"
+                            alt="delete" 
+                            height="14" 
+                            width="14"
+                            onClick={this.onRevertClick.bind(this)} />
+                    </span>
+            );
+        }
+
+        return (
+            <span className="task-item-desc right">{this.props.time.getMonth()}/{this.props.time.getDate()} 
+                        <span> </span>
+                        <img src="../resources/images/edit.svg" 
+                            className="edit-img"
+                            alt="edit" 
+                            height="15" 
+                            width="15"
+                            onClick={this.onEditClick.bind(this)} /> 
+                        <span> </span>
+                        <img src="../resources/images/x.svg" 
+                            className="delete-img"
+                            alt="delete" 
+                            height="14" 
+                            width="14"
+                            onClick={this.onDeleteClick.bind(this)} />
+            </span>
+
+        );
+    }
+    
+    
     render() {
         return (
             <div className="task-item-container">
                 <div className="task-item">
                     <span className="task-item-desc">{this.props.desc}</span>
-                    <span className="task-item-desc right">{this.props.time.getMonth()}/{this.props.time.getDate()} <img src="../resources/images/cross.svg" alt="delete" height="15" width="15" /></span>
+                    {this.renderActionsSection()}
                 </div>
-                <div>
-                    
-                </div>
-                <div>
-                    <button>edit</button>
-                    <button>delete</button>
-                </div> 
             </div>
         );
+    }
+
+    /* 
+    // handler for the edit-pencil click
+    */
+    onEditClick() {
+        this.setState({ isEditing: true });
+    }
+
+    /* 
+    // handler for the delete-pencil click
+    */
+    onDeleteClick() {
+        //add logic for delete
+        console.log('you clicked delete');
+    }
+
+    /* 
+    // handler for the revert click
+    */
+    onRevertClick() {
+        this.setState({ isEditing: false });
     }
 }
